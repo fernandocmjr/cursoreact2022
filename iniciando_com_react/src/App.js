7d216b440915b68d3011a3a5d565bd547b47ac86
Fernando Corrêa de Mello Júnior <fernando.correa@verticalinvestimentos.com>
2022-02-06 14:12:29 -0300

    Produto

--- iniciando_com_react/src/App.js
+++ iniciando_com_react/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
+import React from "react";
+import react from "react";
 import './App.css';
 
 function App() {
+   const criarComboBox = () => {
+    const opcoes = ["Azul", "Vermelho"]
+    const comboBoxOpcoes = opcoes.map( opcao => <option key={opcao}>{opcao}</option>)
+    return ( 
+       <select>
+         {comboBoxOpcoes}
+       </select>
+      )
+  }
+
+  const MeuCombo = () => criarComboBox();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1>Hello Word1</h1>
+       <MeuCombo/>
     </div>
   );
 }
+
+// class App extends react.Component {
+
+//   state = {
+//     nome : "Fernando"
+//   }
+
+//   componentDidMount() {
+//     console.log('DidMount')
+//   }
+//   componentWillUnmount() {
+//     console.log('Unmount')
+//   }
+
+//   modificarNome = (event) => {
+//     this.setState({
+//       nome: event.target.value
+//     })
+//   }
+//   criarComboBox = () => {
+//     const opcoes = ["Azul", "Vermelho"]
+//     const comboBoxOpcoes = opcoes.map( opcao => <option key={opcao}>{opcao}</option>)
+//     return ( 
+//        <select>
+//          {comboBoxOpcoes}
+//        </select>
+//       )
+//   }
+//   render() {
+//     const MeuCombo = () => this.criarComboBox()
+//     console.log('Render')
+//     return (
+//       <>
+//         <div className="App">
+//           <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+//           <h1>Hello {this.state.nome}</h1>
+//           {/* { this.criarComboBox() } */}
+//           <MeuCombo key="combo" />
+//         </div>
+//       </>
+//     );
+//   }
+// }
 
 export default App;
